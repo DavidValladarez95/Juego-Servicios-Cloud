446117676a49f63eaeb8cd4354ebff5efa9fb735
David Fernando Valladarez Muñoz <49535179+DavidValladarez95@users.noreply.github.com>
2020-07-14 19:50:46 -0500

    Implementación fase 1 registro y login
    Sin comentarios

--- ProyectoReconocimientoImagenes/Juego.BL/RegistrationLogicFirebase.cs
+++ ProyectoReconocimientoImagenes/Juego.BL/RegistrationLogicFirebase.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+using System.ComponentModel.Design;
 
 namespace Juego.BL
 {
     class RegistrationLogicFirebase
     {
+
+        public RegistrationLogicFirebase()
+        {
+            InitializeComponent();
+        }
+
+        IFirebaseConfig ifc = new FirebaseConfig()
+        {
+            AuthSecret = "LIXXDJh0Z6D3mP8ZaGNIQuax2SiRZOGxKgrl00Vv",
+            BasePath = "https://juego-servicios-cloud-usuarios.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+        private void InitializeComponent()
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(ifc);
+
+            }
+            catch
+            {
+                Console.WriteLine("Existe un error de conexion - Contacte con el administrador XD ");
+            }
+        }
     }
 }
--- ProyectoReconocimientoImagenes/Juego.BL/loginLogicFirebase.cs
+++ ProyectoReconocimientoImagenes/Juego.BL/loginLogicFirebase.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+using System.ComponentModel.Design;
 
 namespace Juego.BL
 {
-    class Class2
+    class loginLogicFirebase
     {
+        public loginLogicFirebase()
+        {
+            InitializeComponent();
+
+        }
+
+
+        IFirebaseConfig ifc = new FirebaseConfig()
+        {
+            AuthSecret = "LIXXDJh0Z6D3mP8ZaGNIQuax2SiRZOGxKgrl00Vv",
+            BasePath = "https://juego-servicios-cloud-usuarios.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+        private void InitializeComponent()
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(ifc);
+
+            }
+            catch
+            {
+                Console.WriteLine("Existe un error de conexion - Contacte con el administrador XD ");
+            }
+        }
     }
 }
